3162cd880bf0180e60863104b1d48a4152db0e42
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2020-07-07 21:47:01 +0200

    Training2

---
+++ src/main/java/src/training2RelationshipsBetweenEntities/HibernateSession.java
@@ -0,0 +1,21 @@
+package src.training2RelationshipsBetweenEntities;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public enum HibernateSession {
+    INSTANCE;
+
+    public SessionFactory getSessionFactory() {
+        StandardServiceRegistry standardServiceRegistry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+
+        MetadataSources metadataSources = new MetadataSources(standardServiceRegistry);
+        Metadata metadata = metadataSources.getMetadataBuilder().build();
+        return metadata.getSessionFactoryBuilder().build();
+    }
+}
